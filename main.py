2f2ccef2111fd9ef4318f4513ae17d21b1c8dac9
Hilary1232 <39810181+Hilary1232@users.noreply.github.com>
2020-05-26 14:01:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import basic
-print('you tried')
-while True:
-    text = input('basic > ')
-    result, error = basic.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+file1 = open('entrada.txt', 'r') 
+file2 = open('salida.txt', 'w') 
+lines = file1.readlines()
+
+for line in lines:
+    if line:
+    	result, error = basic.run('<stdin>', line)
+    	if error: print(error.as_string())
+    	else:
+    		print(result)
+    		w.write(str(result))
+    
+file1.close()
+file2.close()
