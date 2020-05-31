c9899528e8db90a030f49595f94b126a6029b3a4
Maria <m.venegasb98@gmail.com>
2020-05-31 17:12:06 -0600

    arreglo de errores

--- basic.py
+++ basic.py
@@ -57,13 +57,13 @@ class Posicion:
 
 TOK_PROGRAMA = 'Programa'
 TOK_CLASE_PRINCIPAL = 'ClasePrincipal'
-TOK_IDENTIFICADOR	= 'Identificador'
-TOK_RESERVADA		= 'reservada'
-TOK_ClAS_DEF = 'ClasDef'
+TOK_IDENTIFICADOR   = 'Identificador'
+TOK_RESERVADA       = 'reservada'
+TOK_CLAS_DEF = 'ClasDef'
 TOK_DECL_DEF = 'DeclVar'
 TOK_DECL_MET = 'DeclMet'
 TOK_LISTA_FORMAL = 'ListaFormal'
-TOK_RESTO_FORMAL	= 'RestoFormal'
+TOK_RESTO_FORMAL    = 'RestoFormal'
 TOK_TIPO = 'Tipo'
 TOK_DECLARACION = 'Declaracion'
 TOK_EXPRESION    = 'Expresion'
@@ -72,13 +72,40 @@ TOK_ASIGNACION    = 'asignacion'
 TOK_ENT = 'digitos'
 TOK_COMENT = 'comentario'
 TOK_OPERADOR = 'operador'
+TOK_PARENIZQ = 'parentizq'
+TOK_PARENDER= 'parentder'
+TOK_CORIZQ = 'corcheteizq'
+TOK_CORDER= 'corcheteder'
+
+tipos = ['ent','ent[]','Cadena[]','car[]','flotante[]',
+    'Cadena',
+    'flotante',
+    'doble',
+    'booleano','largo','car','vacio']
 
 reservadas = [
-    'clase',
     'si',
+    'clase',
     'mientras',
     'entonces',
+    'imprimir',  'verdadero',
+    'falso',
+    'esto',
+    'nuevo','ent',
+    'cadena',
+    'flotante', 'publico','privado',
+    'doble',
+    'booleano','car','extiende', 'para','intentar',
+    'finalmente', 'excepto','hacer','continuar','interrumpir','implementa','paquete','retornar','largo','estatico','lanza'
 ]
+clasdecls = [
+    'clase',
+    'extiende']
+exp = [    
+    'verdadero',
+    'falso',
+    'esto',
+    'nuevo']
 
 operadores = [
     '+',
@@ -94,9 +121,10 @@ operadores = [
     '>=',
     '=',
 ]
+ignorar = [';','.',':']
 
 class Token:
-	# Edite la variable tipo y valor, si no funcionan entonces hay que devolverlo a type y value xD
+    # Edite la variable tipo y valor, si no funcionan entonces hay que devolverlo a type y value xD
     def __init__(self, tipo_, valor=None, pos_start=None, pos_end=None):
         self.tipo = tipo_
         self.valor = valor
@@ -127,18 +155,32 @@ class analizadorLexico:
         while self.current_char != None:
             if self.current_char in ' \t':
                 self.avanzar()
+            elif self.current_char in ignorar:
+                self.avanzar()
             elif self.current_char in DIGITOS:
                 tokens.append(self.crear_numero())
             elif self.current_char in LETRAS:
-                tokens.append(self.crear_identificador())
+                tokens = self.crear_identificador(tokens)
+                print(tokens)
             elif self.current_char in operadores:
                 tokens.append(Token(TOK_OPERADOR, self.current_char))
                 self.avanzar()
             elif self.current_char == '/':
                 tokens.append(self.crear_comentario())
                 self.avanzar()
+            elif self.current_char == '(':
+                tokens.append(Token(TOK_PARENIZQ,self.current_char))
+                self.avanzar()
             elif self.current_char == ')':
-                tokens.append(self.crear_comentario)
+                tokens.append(Token(TOK_PARENDER,self.current_char))
+                self.avanzar()
+            elif self.current_char == '\n':
+                self.avanzar()
+            elif self.current_char == '{':
+                tokens.append(Token(TOK_CORIZQ,self.current_char))
+                self.avanzar()
+            elif self.current_char == '}':
+                tokens.append(Token(TOK_CORDER,self.current_char))
                 self.avanzar()
             elif self.current_char == '\n':
                 self.avanzar()
@@ -147,7 +189,7 @@ class analizadorLexico:
                 char = self.current_char
                 self.avanzar()
                 mensaje = Error(pos_start, self.pos, "Reporte de Error", "'" + char + "'")
-                return [], mensaje
+                return tokens, mensaje
                 
 
         return tokens, None
@@ -155,21 +197,43 @@ class analizadorLexico:
 
 
 
-    def crear_identificador(self):
+    def crear_identificador(self,tokens):
         identi = ''
         inicio = self.pos.copiar()
         bandera = False
         while bandera == False:
-            if(self.current_char != None and self.current_char in LETTERS_DIGITS + '_'):
+            print(self.current_char)
+            if(self.current_char != None and self.current_char in LETTERS_DIGITS + '_') or (self.current_char == '[') or (self.current_char == ']'):
                 identi += self.current_char
                 self.avanzar()
 
             else:
+                print("llegue")
+                print(identi)
                 bandera = True
-                self.avanzar()
 
-        tok_type = TOK_RESERVADA if identi in reservadas else TOK_IDENTIFICADOR
-        return Token(tok_type, identi, inicio, self.pos)
+        if identi == 'principal':
+            tok_type = TOK_CLASE_PRINCIPAL
+            tokens.append(Token(tok_type, identi, inicio, self.pos))
+        if identi in clasdecls:
+            tok_type = TOK_CLAS_DEF
+            tokens.append(Token(tok_type, identi, inicio, self.pos))
+        if identi == 'public':
+            tok_type = TOK_DeclMet
+            tokens.append(Token(tok_type, identi, inicio, self.pos))    
+        if identi in exp:
+            tok_type = TOK_EXPRESION
+            tokens.append(Token(tok_type, identi, inicio, self.pos))
+        if identi in tipos:
+            tok_type = TOK_TIPO
+            tokens.append(Token(tok_type, identi, inicio, self.pos))
+        if identi in reservadas:
+            tok_type = TOK_RESERVADA
+            tokens.append(Token(tok_type, identi, inicio, self.pos))
+        else:
+            tok_type = TOK_IDENTIFICADOR
+            tokens.append(Token(tok_type, identi, inicio, self.pos))
+        return tokens
     
     
     def crear_numero(self):
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import basic
-
-file1 = open('entrada.txt', 'r') 
-file2 = open('salida.txt', 'w') 
+import sys
+file1 = open(sys.argv[1], 'r') 
+file2 = open(sys.argv[2], 'w+') 
 lines = file1.readlines()
-num_linea = 1
+num_linea = -1
 
 for line in lines:
     if line:
         result, error = basic.run('<stdin>', line, num_linea)
         num_linea += 1
-    if error: print(error.as_string())
-    else:
-        print(result)
+    if error: 
+        #print(result)
         for i in range(0,len(result)):
             file2.write('<'+str(result[i])+'>')
             file2.write("   ")
+        print(error.as_string())
+        exit()
 
-        file2.write('\n')
-    
+    file2.write('\n')
+    #print(result)
+    for i in range(0,len(result)):
+        file2.write('<'+str(result[i])+'>')
+        file2.write("   ")
+
+    file2.write('\n')
+
 file1.close()
 file2.close()
+exit()
                     
